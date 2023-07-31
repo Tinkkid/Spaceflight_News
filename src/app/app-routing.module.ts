4ba9ec53aa0f957b6b9da557cde6abc68a5eff70
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-07-31 16:56:42 +0300

    Created: test-card for article and added some styles for footer and header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticleItemComponent } from './components/article-item/article-item.component';
+import { ArticlesListComponent } from './components/articles-list/articles-list.component';
 
 const routes: Routes = [
-  {path:'',}
+  { path: '', component: ArticlesListComponent },
+  { path: 'article/:id', component: ArticleItemComponent },
+  {path:'**', redirectTo: '', component: ArticlesListComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { HeaderComponent } from './components/UI/header/header.component';
 import { FooterComponent } from './components/UI/footer/footer.component';
 import { ArticlesListComponent } from './components/articles-list/articles-list.component';
 import { ArticleItemComponent } from './components/article-item/article-item.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -15,14 +19,17 @@ import { ArticleItemComponent } from './components/article-item/article-item.com
     HeaderComponent,
     FooterComponent,
     ArticlesListComponent,
-    ArticleItemComponent
+    ArticleItemComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
